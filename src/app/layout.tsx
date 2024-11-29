7d953bee14413b98b691a3dcc26e075c3ee8cc72
ShoyaMatsuzaki <s.matsuzaki@hiroka.biz>
2024-11-29 16:29:25 +0900

    chore:一旦AD認証はずした

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,19 @@
+// app/layout.tsx
+/* eslint-disable react/react-in-jsx-scope */
 'use client'; // クライアントコンポーネント
 import React, { useEffect } from 'react';
 import ResponsiveAppBar from '@/components/ResponsiveAppBar';
 import './globals.css'; // 共通CSSの読み込み
 import { ReactNode } from 'react';
-import { UserProvider, useUser } from './context/UserContext';
-import { useSession, SessionProvider, signIn } from 'next-auth/react';
+import { User, UserProvider, useUser } from './context/UserContext';
+
+const mockUserData: User = {
+  user_name: '松崎 祥也',
+  login_code: 'ShoyaMatsuzaki',
+  department: '開発チーム',
+  companyts: 'HIROKA',
+  email: 's.matsuzaki@hiroka.biz',
+};
 
 export default function RootLayout({
   children,
@@ -14,11 +23,9 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body>
-        <SessionProvider>
-          <UserProvider>
-            <MainContent>{children}</MainContent> {/* 子コンポーネントを渡す */}
-          </UserProvider>
-        </SessionProvider>
+        <UserProvider>
+          <MainContent>{children}</MainContent> {/* 子コンポーネントを渡す */}
+        </UserProvider>
       </body>
     </html>
   );
@@ -27,46 +34,18 @@ export default function RootLayout({
 // メインコンテンツでユーザー情報を渡す
 const MainContent: React.FC<{ children: ReactNode }> = ({ children }) => {
   const { setUser } = useUser(); // UserContextからsetUserを取得
-  const { data: session, status } = useSession(); // next-authのセッションを取得
-  console.log('Session:', session);
-  console.log('Status:', status);
 
   useEffect(() => {
-    if (status === 'authenticated' && session?.user) {
-      // session.userにEmail情報が含まれているので、setUserを使って更新する
-      setUser({
-        email: session.user.email || '', // emailが存在すれば設定
-        user_name: session.user.name || '', // ユーザー名も取得できれば設定
-        login_code: '999', // login_codeがあれば設定、なければ空文字
-        department: 'test', // departmentがあれば設定、なければ空文字
-        companyts: 'test co', // companytsがあれば設定、なければ空文字
-      });
-    }
-  }, [session, status, setUser]);
+    // EntraIDから取得した情報を仮データで設定
+    setUser(mockUserData);
+  }, [setUser]);
 
   const { user } = useUser(); // UserContextからユーザー情報を取得
-
-  if (status === 'loading') {
-    return <p>Loading...</p>; // セッションをロード中
-  }
-
-  if (status === 'unauthenticated') {
-    return (
-      <>
-        <p>Not signed in</p>
-        <button onClick={() => signIn('azure-ad')}>Sign in with Azure AD</button>
-      </>
-    );
-  }
 
   return (
     <>
       <ResponsiveAppBar userName={user ? user.user_name : null} /> {/* ユーザー名を渡す */}
-      <main>
-        {/* ここでユーザーのEmailも表示 */}
-        {user?.email && <p>Email: {user.email}</p>}
-        {children} {/* 各ページのコンテンツ */}
-      </main>
+      <main>{children}</main> {/* 各ページのコンテンツ */}
     </>
   );
 };

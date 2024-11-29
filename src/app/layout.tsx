0a4f46ab154fd929110326d87a5f3f2e6b7d9f28
ShoyaMatsuzaki <s.matsuzaki@hiroka.biz>
2024-11-29 14:36:24 +0900

    feat:AD認証

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,11 @@
 // app/layout.tsx
-/* eslint-disable react/react-in-jsx-scope */
 'use client'; // クライアントコンポーネント
 import React, { useEffect } from 'react';
 import ResponsiveAppBar from '@/components/ResponsiveAppBar';
 import './globals.css'; // 共通CSSの読み込み
 import { ReactNode } from 'react';
 import { User, UserProvider, useUser } from './context/UserContext';
-
-const mockUserData: User = {
-  user_name: '松崎 祥也',
-  login_code: 'ShoyaMatsuzaki',
-  department: '開発チーム',
-  company: 'HIROKA',
-};
+import { useSession, SessionProvider, signIn } from 'next-auth/react';
 
 export default function RootLayout({
   children,
@@ -22,9 +15,11 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body>
-        <UserProvider>
-          <MainContent>{children}</MainContent> {/* 子コンポーネントを渡す */}
-        </UserProvider>
+        <SessionProvider>
+          <UserProvider>
+            <MainContent>{children}</MainContent> {/* 子コンポーネントを渡す */}
+          </UserProvider>
+        </SessionProvider>
       </body>
     </html>
   );
@@ -33,13 +28,48 @@ export default function RootLayout({
 // メインコンテンツでユーザー情報を渡す
 const MainContent: React.FC<{ children: ReactNode }> = ({ children }) => {
   const { setUser } = useUser(); // UserContextからsetUserを取得
+  const { data: session, status } = useSession(); // next-authのセッションを取得
+  console.log('Session:', session);
+  console.log('Status:', status);
 
   useEffect(() => {
-    // EntraIDから取得した情報を仮データで設定
-    setUser(mockUserData);
-  }, [setUser]);
+    if (status === 'authenticated' && session?.user) {
+      // Microsoft Graph APIからユーザー情報を取得
+      const fetchUserData = async () => {
+        try {
+          const res = await fetch('/api/fetchUserDetails', {
+            method: 'GET',
+            headers: {
+              Authorization: `Bearer ${session.user.accessToken}`,
+            },
+          });
+          if (!res.ok) throw new Error('Failed to fetch user data');
+
+          const userData: User = await res.json();
+          setUser(userData); // UserContextに設定
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+      fetchUserData();
+    }
+  }, [session, status, setUser]);
 
   const { user } = useUser(); // UserContextからユーザー情報を取得
+
+  if (status === 'loading') {
+    return <p>Loading...</p>; // セッションをロード中
+  }
+
+  if (status === 'unauthenticated') {
+    return (
+      <>
+        <p>Not signed in</p>;
+        <button onClick={() => signIn('azure-ad')}>Sign in with Azure AD</button>
+      </>
+    );
+  }
 
   return (
     <>

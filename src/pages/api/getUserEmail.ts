bca0d9617f4f0bc92d4af469f51da4fe9c4bde35
ShoyaMatsuzaki <s.matsuzaki@hiroka.biz>
2024-12-23 17:24:15 +0900

    tmp

--- src/pages/api/getUserEmail.ts
+++ src/pages/api/getUserEmail.ts
@@ -4,7 +4,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method === 'GET') {
     // リクエストヘッダー全体をコンソールに出力
     console.log('Request Headers:', req.headers);
-
+    const accessToken = req.headers['x-ms-token-aad-access-token'];
+    console.log('accessToken:', accessToken);
+    const idToken = req.headers['x-ms-token-aad-id-token'];
+    console.log('idToken:', idToken);
     const userEmail = req.headers['x-ms-client-principal-name'] || null;
     console.log('User email:', userEmail);
 

b3c65566a8c30ec6bb0f3a6ba9ae08be8618932a
ShoyaMatsuzaki <s.matsuzaki@hiroka.biz>
2024-12-24 17:28:54 +0900

    feat:小画面時のアプリ名クリックでホームへ戻る

--- src/components/ResponsiveAppBar.tsx
+++ src/components/ResponsiveAppBar.tsx
@@ -127,7 +127,7 @@ const ResponsiveAppBar: React.FC<Props> = ({ userName }) => {
             variant="h5"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/"
             sx={{
               mr: 2,
               display: { xs: 'flex', md: 'none' },
